b8999ac86f73d9b999fe1305ec70344d1bd53384
David Faden (revfad.com) <dfaden@gmail.com>
2025-08-27 12:52:45 -0700

    Add ability to edit the mosaic
    Includes controls to change the width and height as well
    as to clear it. Not the best drawing experience still.
    
    The code might be a lot nicer with a class-based design.

--- mosaic.js
+++ mosaic.js
@@ -30,48 +30,71 @@ const FACE = [
   "00000000000000",
 ];
 
+let state = null;
+
 // Pixels size (width/height) for the individual GIFs.
 const IMAGE_PIXEL_SIZE = 20;
 
-function createMosaic(parentElem, rows) {
+function updateState(event) {
+  event.stopPropagation();
+  const checkbox = event.target;
+  const rowIndex = checkbox.dataset.rowIndex;
+  const colIndex = checkbox.dataset.colIndex;
+  if (typeof rowIndex === "undefined" || typeof colIndex === "undefined") {
+    console.log("Unexpected target", target);
+  }
+  const cells = state[rowIndex].split("");
+  cells[colIndex] = checkbox.checked ? "1" : "0";
+  state[rowIndex] = cells.join("");
+}
+
+function drawMosaic(parentElem, rows, editParams = null) {
+  parentElem.innerHTML = "";
   const table = document.createElement("table");
   table.classList.add("mosaic-table");
+  let width = rows[0].length;
+  let height = rows.length;
+  if (editParams) {
+    table.classList.add("mosaic-table-editing");
+    ({ width, height } = editParams);
+  }
   table.style.width = rows[0].length * IMAGE_PIXEL_SIZE;
   table.style.height = rows.length * IMAGE_PIXEL_SIZE;
 
-  for (const row of rows) {
+  for (let i = 0; i < height; i++) {
+    const row = rows[i];
     const tr = document.createElement("tr");
-    for (const c of row) {
-      const src = c === "0" ? "images/white.gif" : "images/black.gif";
+    for (let j = 0; j < width; j++) {
+      const c = row[j];
       const td = document.createElement("td");
-      const img = document.createElement("img");
-      img.width = IMAGE_PIXEL_SIZE;
-      img.height = IMAGE_PIXEL_SIZE;
-      img.src = src;
-      img.alt = c;
-      td.appendChild(img);
+      const on = c === "1";
+      if (editParams) {
+        const label = document.createElement("label");
+        label.classList.add("mosaic-edit-label");
+        const checkbox = document.createElement("input");
+        checkbox.classList.add("mosaic-checkbox");
+        checkbox.addEventListener("change", updateState);
+        checkbox.dataset.rowIndex = i;
+        checkbox.dataset.colIndex = j;
+        checkbox.type = "checkbox";
+        checkbox.checked = on;
+        label.appendChild(checkbox);
+        td.appendChild(label);
+      } else {
+        const src = on ? "images/black.gif" : "images/white.gif";
+        const img = document.createElement("img");
+        img.width = IMAGE_PIXEL_SIZE;
+        img.height = IMAGE_PIXEL_SIZE;
+        img.src = src;
+        img.alt = c;
+        td.appendChild(img);
+      }
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
   parentElem.appendChild(table);
 }
-
-const soundCheckbox = document.getElementById("sound-checkbox");
-const soundPlayer = document.getElementById("sound-player");
-
-soundCheckbox.addEventListener("change", async function () {
-  if (soundCheckbox.checked) {
-    try {
-      await soundPlayer.play();
-    } catch (err) {
-      console.log(err);
-      soundCheckbox.checked = false;
-    }
-  } else {
-    soundPlayer.pause();
-  }
-});
 
 function parseMosaicRows(mosaicParam) {
   if (!mosaicParam) {
@@ -101,7 +124,93 @@ function parseMosaicRows(mosaicParam) {
   return rows;
 }
 
-createMosaic(
-  document.getElementById("mosaic"),
-  parseMosaicRows(new URLSearchParams(location.search).get("m")) || FACE,
-);
+const mosaicElem = document.getElementById("mosaic");
+const soundCheckbox = document.getElementById("sound-checkbox");
+const soundPlayer = document.getElementById("sound-player");
+const editButton = document.getElementById("edit-button");
+const editControl = document.getElementById("edit-controls");
+const widthInput = document.getElementById("width");
+const heightInput = document.getElementById("height");
+const clearButton = document.getElementById("clear-button");
+const doneButton = document.getElementById("done-button");
+
+state = parseMosaicRows(new URLSearchParams(location.search).get("m")) || [
+  ...FACE,
+];
+
+soundCheckbox.addEventListener("change", async function () {
+  if (soundCheckbox.checked) {
+    try {
+      await soundPlayer.play();
+    } catch (err) {
+      console.log(err);
+      soundCheckbox.checked = false;
+    }
+  } else {
+    soundPlayer.pause();
+  }
+});
+
+editButton.addEventListener("click", function () {
+  editControl.style.display = "";
+  widthInput.value = state[0].length;
+  heightInput.value = state.length;
+  drawMosaic(mosaicElem, state, {
+    width: widthInput.value,
+    height: heightInput.value,
+  });
+});
+
+function handleDimensionChange() {
+  const width = parseInt(widthInput.value);
+  const height = parseInt(heightInput.value);
+  while (state.length < height) {
+    state.push("0".repeat(width));
+  }
+  for (let i = 0; i < state.length; i++) {
+    let widthDiff = width - state[i].length;
+    if (widthDiff > 0) {
+      state[i] = state[i] + "0".repeat(widthDiff);
+    }
+  }
+  drawMosaic(mosaicElem, state, { width, height });
+}
+
+widthInput.addEventListener("change", handleDimensionChange);
+heightInput.addEventListener("change", handleDimensionChange);
+
+clearButton.addEventListener("click", function () {
+  for (let i = 0; i < state.length; i++) {
+    state[i] = "0".repeat(state[i].length);
+  }
+  drawMosaic(mosaicElem, state, {
+    width: widthInput.value,
+    height: heightInput.value,
+  });
+});
+
+doneButton.addEventListener("click", function () {
+  const width = parseInt(widthInput.value);
+  const height = parseInt(heightInput.value);
+  if (state.length > height) {
+    state = state.slice(0, height);
+  }
+  for (let i = 0; i < state.length; i++) {
+    const row = state[i];
+    if (row.length > width) {
+      state[i] = row.substring(0, width);
+    }
+  }
+  const serialized = BigInt(
+    "0b" +
+      state
+        .map((row) => row.split("").toReversed().join(""))
+        .toReversed()
+        .join(""),
+  );
+  const mosaicParam = `${width}x${height},${serialized.toString(16)}`;
+  const baseUrl = location.href.replace(/#.*/, "").replace(/\?.*/, "");
+  location.href = `${baseUrl}?m=${mosaicParam}`;
+});
+
+drawMosaic(mosaicElem, state);
